0171d131bf2c04b757f8c83ac394dbd2e5e35ae2
mangi77 <130028440+mangi77@users.noreply.github.com>
2023-04-27 02:17:49 +0530

    Aws cli

--- aws-cli.sh
+++ aws-cli.sh
@@ -4,8 +4,8 @@ sudo apt install apache2 -y
 sudo systemctl start apache2
 sudo apt install unzip -y
 echo "[default]" > /.aws/credentials
-echo "aws_access_key_id = AKIA6KN65HRR2CX4XGW2" >> /.aws/credentials
-echo "aws_secret_access_key = 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" >> /.aws/credentials
+echo "aws_access_key_id = "access key" >> /.aws/credentials
+echo "aws_secret_access_key = "secret access key " >> /.aws/credentials
 echo "[default]" > /.aws/config
 echo "region = us-east-1" >> /.aws/config
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
